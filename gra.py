2733fb87ebcd6600eec75acaba265026924d2c29
Łukasz Malinowski <lukasz.malinowski79@gmail.com>
2023-06-05 23:31:09 +0200

    Edycja pliku gra.py

--- gra.py
+++ gra.py
@@ -7,24 +7,37 @@ def roll_dice():
 
 def move_player(player, steps):
     player["position"] += steps
-    if player["position"] >= 50:
-        player["level"] += 1
-        messagebox.showinfo("Awans na poziom", f"Gracz {player['name']} awansował na poziom {player['level']}")
-        player["position"] -= 50
+    if player["level"] <= 5:
+        improve = player["level"] * 30
+        if player["position"] >= improve:
+            player["level"] += 1
+            messagebox.showinfo("Awans na poziom", f"Gracz {player['name']} awansował na poziom {player['level']}")
+    else:
+        improve = (player["level"] * 30) + ((player["level"]-5)*20)
+        
+
 
 def process_event(player, players):
-    event = random.randint(1, 3)
-    if event == 1:
-        messagebox.showinfo("Skarb!", f"Gracz {player['name']} znalazł skarb! Otrzymuje dodatkowe 100 punktów.")
+    event = random.choices(
+        population=["Alkowalka", "Globalne", "Personalne", "Puste pole", "Bar"],
+        weights=[0.1, 0.05, 0.25, 0.5, 0.1],
+        k=1
+    )[0]
+
+    if event == "Alkowalka":
+        messagebox.showinfo("Alkowalka!", f"Gracz {player['name']} musi stoczyć pojedynek z wybraną przez niego osobą.")
         player["score"] += 100
-    elif event == 2:
-        messagebox.showinfo("Przeciwnik!", f"Gracz {player['name']} natknął się na przeciwnika! Traci 50 punktów.")
+    elif event == "Globalne":
+        messagebox.showinfo("Globalne!", f"Gracz {player['name']} natknął się na Globalne wydarzenie! Traci 50 punktów.")
         player["score"] -= 50
-    elif event == 3:
-        messagebox.showinfo("Dodatkowy ruch!", f"Gracz {player['name']} wejdzie na pole 31 i otrzymuje dodatkowy ruch.")
+    elif event == "Personalne":
+        messagebox.showinfo("Personalne!", f"Gracz {player['name']} spotyka osobiste wydarzenie! Otrzymuje dodatkowy ruch.")
         play_game(players)  # Wywołujemy rekurencyjnie funkcję play_game dla tego samego gracza
-    else:
-        messagebox.showinfo("Kontynuacja podróży", f"Gracz {player['name']} kontynuuje podróż.")
+    elif event == "Puste pole":
+        messagebox.showinfo("Puste pole!", f"Gracz {player['name']} wchodzi na puste pole. Nic się nie dzieje.")
+    elif event == "Bar":
+        messagebox.showinfo("Bar!", f"Gracz {player['name']} trafia do baru! Otrzymuje dodatkowe 50 punktów.")
+        player["score"] += 50
 
 def play_game(players):
     for player in players:
