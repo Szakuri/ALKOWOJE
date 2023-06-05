0f0df78b68881cd1b31c8f311cf0c24f6c4b3abe
Łukasz Malinowski <lukasz.malinowski79@gmail.com>
2023-06-05 16:33:26 +0200

    Stworzenie szkieletu gry planszowej

---
+++ gra.py
@@ -0,0 +1,69 @@
+import random
+import tkinter as tk
+from tkinter import messagebox
+
+def roll_dice():
+    return random.randint(0, 10)
+
+def move_player(player, steps):
+    player["position"] += steps
+    if player["position"] >= 50:
+        player["level"] += 1
+        messagebox.showinfo("Awans na poziom", f"Gracz {player['name']} awansował na poziom {player['level']}")
+        player["position"] -= 50
+
+def process_event(player):
+    event = random.randint(1, 3)
+    if event == 1:
+        messagebox.showinfo("Skarb!", f"Gracz {player['name']} znalazł skarb! Otrzymuje dodatkowe 100 punktów.")
+        player["score"] += 100
+    elif event == 2:
+        messagebox.showinfo("Przeciwnik!", f"Gracz {player['name']} natknął się na przeciwnika! Traci 50 punktów.")
+        player["score"] -= 50
+    else:
+        messagebox.showinfo("Kontynuacja podróży", f"Gracz {player['name']} kontynuuje podróż.")
+
+def play_game(players):
+    while True:
+        for player in players:
+            messagebox.showinfo("Tura gracza", f"Tura gracza {player['name']}")
+            roll = roll_dice()
+            messagebox.showinfo("Wyrzucono", f"Wyrzucono: {roll}")
+            move_player(player, roll)
+            process_event(player)
+            messagebox.showinfo("Aktualny poziom", f"Aktualny poziom gracza {player['name']}: {player['level']}")
+            messagebox.showinfo("Aktualny wynik", f"Aktualny wynik gracza {player['name']}: {player['score']}")
+
+            if player["level"] >= 10:
+                messagebox.showinfo("Wygrana!", f"Gracz {player['name']} osiągnął maksymalny poziom!")
+                return
+
+def start_game():
+    num_players = int(num_players_var.get())
+    players = []
+
+    for i in range(num_players):
+        player_name = f"Gracz {i + 1}"
+        player = {"name": player_name, "level": 1, "position": 0, "score": 0}
+        players.append(player)
+
+    play_game(players)
+
+# Tworzenie GUI
+window = tk.Tk()
+window.title("Gra planszowa")
+window.geometry("300x200")
+
+num_players_label = tk.Label(window, text="Liczba graczy (1-8):")
+num_players_label.pack()
+
+num_players_var = tk.StringVar(window)
+num_players_var.set("2")
+
+num_players_menu = tk.OptionMenu(window, num_players_var, "1", "2", "3", "4", "5", "6", "7", "8")
+num_players_menu.pack()
+
+start_button = tk.Button(window, text="Rozpocznij grę", command=start_game)
+start_button.pack()
+
+window.mainloop()
